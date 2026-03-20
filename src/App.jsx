2eae1dc7c1dd2cccd7026a86028a3845bbce38fe
HenokSebsibe <henoksebsibe12@gmail.com>
2026-03-20 05:01:23 -0700

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-﻿import "./App.css";
-
-function Apps() {
+﻿import { Routes, Route } from "react-router-dom";
+import Home from "./assets/pages/Home.jsx";
+import Auth from "./assets/pages/auth.jsx";
+import Checkout from "./assets/pages/Checkout.jsx";
+import Navbar from "./components/Navbar";
+function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<SignupForm />} />
+        <Route path="/auth"  element={<Auth />} />
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </div>
-  )
+  );
 }
+
+export default App;
---
+++ src/assets/pages/Auth.jsx
@@ -0,0 +1,7 @@
+export default function Auth() {
+    return (
+        <div >
+        <h1>Authentication Page</h1>
+        </div>
+    );
+}
